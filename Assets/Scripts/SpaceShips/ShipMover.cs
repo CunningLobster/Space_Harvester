958d95f857e86c068cc52f484de76b21aa0036f3
CunningLobster <cherepansky.yan@yandex.ru>
2022-01-09 00:06:09 +0300

    Added Base Logic

--- Assets/Scripts/SpaceShips/ShipMover.cs
+++ Assets/Scripts/SpaceShips/ShipMover.cs
@@ -22,7 +22,8 @@ namespace SpaceCarrier.SpaceShips
             rb.velocity = new Vector3(inputProvider.moveDirection.x, 0, inputProvider.moveDirection.y) * speed;
             if (inputProvider.moveDirection != Vector2.zero)
             {
-                transform.rotation = Quaternion.Euler(0, 0, 0);
+                float g = Mathf.Sqrt(Mathf.Pow(inputProvider.moveDirection.x, 2f) + Mathf.Pow(inputProvider.moveDirection.y, 2f)); 
+                transform.rotation = Quaternion.Euler(0, Mathf.Atan2(inputProvider.moveDirection.x, inputProvider.moveDirection.y) / Mathf.PI * 180, 0);
             }
         }
     }

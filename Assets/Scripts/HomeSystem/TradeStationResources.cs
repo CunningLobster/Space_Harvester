ebf59ac2688c74d0c4b94b951cdd25c667a45b6e
CunningLobster <cherepansky.yan@yandex.ru>
2022-01-30 12:18:59 +0300

    Made dictionary based trade station resources

--- Assets/Scripts/HomeSystem/TradeStationResources.cs
+++ Assets/Scripts/HomeSystem/TradeStationResources.cs
@@ -1,5 +1,7 @@
+using SpaceCarrier.Celestials;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace SpaceCarrier.HomeSystem
@@ -12,21 +14,39 @@ namespace SpaceCarrier.HomeSystem
         [SerializeField] private int greenSellingCost;
         [SerializeField] private int brownSellingCost;
 
+        Dictionary<ResourceTypes, int> resourcesSellingCosts = new Dictionary<ResourceTypes, int>(); 
+        Dictionary<ResourceTypes, int> resourcesBuyingCosts = new Dictionary<ResourceTypes, int>(); 
+
         [SerializeField] private float buySellRatio = 1.3f;
 
         [SerializeField] ResourcePanel buyingPart;
         [SerializeField] ResourcePanel sellingPart;
 
+        private void Awake()
+        {
+            resourcesSellingCosts[ResourceTypes.Purple] = purpleSellingCost;
+            resourcesSellingCosts[ResourceTypes.Red] = redSellingCost;
+            resourcesSellingCosts[ResourceTypes.Blue] = blueSellingCost;
+            resourcesSellingCosts[ResourceTypes.Green] = greenSellingCost;
+            resourcesSellingCosts[ResourceTypes.Brown] = brownSellingCost;
+
+            resourcesBuyingCosts = GetBuyingCosts(resourcesSellingCosts);
+        }
         private void Start()
         {
-            sellingPart.UpdatePanel(purpleSellingCost, redSellingCost, blueSellingCost, greenSellingCost, brownSellingCost);
-            buyingPart.UpdatePanel(GetBuyingCost(purpleSellingCost), GetBuyingCost(redSellingCost), GetBuyingCost(blueSellingCost),
-                GetBuyingCost(greenSellingCost), GetBuyingCost(brownSellingCost));
+            sellingPart.UpdatePanel(resourcesSellingCosts);
+            buyingPart.UpdatePanel(resourcesBuyingCosts);
         }
 
-        int GetBuyingCost(int sellingCost)
+        Dictionary<ResourceTypes, int> GetBuyingCosts(Dictionary<ResourceTypes, int> sellingCosts)
         {
-            return Mathf.FloorToInt((float)sellingCost * buySellRatio);
+            Dictionary<ResourceTypes, int> buyingCosts = new Dictionary<ResourceTypes, int>();
+
+            foreach(var key in sellingCosts.Keys.ToList())
+            {
+                buyingCosts[key] = Mathf.FloorToInt((float)sellingCosts[key] * buySellRatio); 
+            }
+            return buyingCosts;
         }
     }
 }

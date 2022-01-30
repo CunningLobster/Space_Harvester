7bfe8d1838578ccc22e388ce4fb47dec04c0b27c
CunningLobster <cherepansky.yan@yandex.ru>
2022-01-30 20:52:11 +0300

    commit

--- Assets/Scripts/HomeSystem/TransactionButtonPanel.cs
+++ Assets/Scripts/HomeSystem/TransactionButtonPanel.cs
@@ -3,30 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TransactionButtonPanel : MonoBehaviour
+namespace SpaceCarrier.HomeSystem
 {
-    [SerializeField] Button Sell1Button;
-    [SerializeField] Button Sell10Button;
-    [SerializeField] Button Sell100Button;
-<<<<<<< HEAD
+    public class TransactionButtonPanel : MonoBehaviour
+    {
+        [SerializeField] Button Sell1Button;
+        [SerializeField] Button Sell10Button;
+        [SerializeField] Button Sell100Button;
 
-    [SerializeField] Button Buy1Button;
-    [SerializeField] Button Buy10Button;
-    [SerializeField] Button Buy100Button;
+        [SerializeField] Button Buy1Button;
+        [SerializeField] Button Buy10Button;
+        [SerializeField] Button Buy100Button;
 
-    [SerializeField] Button DealButton;
-    [SerializeField] Button SellAllButton;
-
-
+        [SerializeField] Button DealButton;
+        [SerializeField] Button SellAllButton;
+    }
 }
-=======
-
-    [SerializeField] Button Buy1Button;
-    [SerializeField] Button Buy10Button;
-    [SerializeField] Button Buy100Button;
-
-    [SerializeField] Button DealButton;
-    [SerializeField] Button SellAllButton;
-
-}
->>>>>>> try-to-made-dictionary-based-panels

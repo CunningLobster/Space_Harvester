725cf4b7fc4a738e8656983632cee644652bd7f6
CunningLobster <cherepansky.yan@yandex.ru>
2022-01-31 17:07:48 +0300

    Try make prefs keys class

--- Assets/Scripts/Controls/InputProvider.cs
+++ Assets/Scripts/Controls/InputProvider.cs
@@ -7,9 +7,9 @@ namespace SpaceCarrier.Controls
     public class InputProvider : MonoBehaviour
     {
         public Vector2 moveDirection { get; private set; }
-        public bool isJumping; /*{ get; private set; }*/
+        public bool isJumping { get; private set; }
 
-        public void OnMove(InputAction.CallbackContext context)
+    public void OnMove(InputAction.CallbackContext context)
         {
             moveDirection = context.ReadValue<Vector2>();
         }
---
+++ Assets/Scripts/PrefsKeys/PrefsKeys.cs
@@ -0,0 +1,19 @@
+namespace SpaceCarrier.Prefs
+{
+    public class PrefsKeys
+    {
+        public const string creditsKey = "Credits_Key";
+
+        public const string s_purpleKey = "S_Purple_Key";
+        public const string s_redKey = "S_Red_Key";
+        public const string s_blueKey = "S_Blue_Key";
+        public const string s_greenKey = "S_Green_Key";
+        public const string s_brownKey = "S_Brown_Key";
+
+        public const string h_purpleKey = "H_Purple_Key";
+        public const string h_redKey = "H_Red_Key";
+        public const string h_blueKey = "H_Blue_Key";
+        public const string h_greenKey = "H_Green_Key";
+        public const string h_brownKey = "H_Brown_Key";
+    }
+}

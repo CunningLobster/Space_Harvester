4d30ee9fcd2bc00d5d677ab7f14740bd07ebfcb4
CunningLobster <cherepansky.yan@yandex.ru>
2022-02-01 21:56:24 +0300

    Made stats scriptable objs

---
+++ Assets/Scripts/ShipStats/ShipStat.cs
@@ -0,0 +1,27 @@
+using SpaceCarrier.Prefs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceCarrier.ShipStats
+{
+    [CreateAssetMenu(fileName = "Stat", menuName = "ScriptableObjects/SpawnShipStat", order = 1)]
+    public class ShipStat : ScriptableObject
+    {
+        [SerializeField] Stats type = Stats.Engine;
+        [SerializeField] private float[] values = new float[10];
+
+        private int currentLevel;
+
+        private void Awake()
+        {
+            currentLevel = PlayerPrefs.GetInt(PrefsKeys.statsKeys[type], 0);
+        }
+
+        public void ChangeLevel(int value)
+        { 
+            PlayerPrefs.SetInt(PrefsKeys.statsKeys[type], value);
+            currentLevel = value;
+        }
+    }
+}

397bb52b845910735d726fb7081753c823e182ea
CunningLobster <cherepansky.yan@yandex.ru>
2022-02-02 07:04:47 +0300

    Made Upgrade system without prices

---
+++ Assets/Scripts/Shipyard/UpgradeButtonPanel.cs
@@ -0,0 +1,28 @@
+using SpaceCarrier.ShipStats;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpaceCarrier.Shipyard
+{
+    public class UpgradeButtonPanel : MonoBehaviour
+    {
+        [SerializeField] private Button engineUpgradeButton;
+        [SerializeField] private Button maneurabilityUpgradeButton;
+        [SerializeField] private Button massUpgradeButton;
+        [SerializeField] private Button cargoCapacityUpgradeButton;
+        [SerializeField] private Button harvestingUpgradeButton;
+
+        [SerializeField] private ShipyardStats shipyardStats;
+
+        private void Awake()
+        {
+            engineUpgradeButton.onClick.AddListener(delegate { shipyardStats.OnUpgradeStat(Stats.Engine); });
+            maneurabilityUpgradeButton.onClick.AddListener(delegate { shipyardStats.OnUpgradeStat(Stats.Maneurability); });
+            massUpgradeButton.onClick.AddListener(delegate { shipyardStats.OnUpgradeStat(Stats.Mass); });
+            cargoCapacityUpgradeButton.onClick.AddListener(delegate { shipyardStats.OnUpgradeStat(Stats.CargoCapacity); });
+            harvestingUpgradeButton.onClick.AddListener(delegate { shipyardStats.OnUpgradeStat(Stats.Harvesting); });
+        }
+    }
+}

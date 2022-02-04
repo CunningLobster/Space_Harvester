7bdad3b30e383f9e7795eb84d48dfbbbc077ff57
CunningLobster <cherepansky.yan@yandex.ru>
2022-02-04 07:12:06 +0300

    Added Resource Cheat

---
+++ Assets/Scripts/ResourceCheat.cs
@@ -0,0 +1,27 @@
+#if UNITY_EDITOR
+using SpaceCarrier.Prefs;
+using SpaceCarrier.Resoures;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceCheat : MonoBehaviour
+{
+    [SerializeField] int credits;
+    [SerializeField] int purple;
+    [SerializeField] int red;
+    [SerializeField] int blue;
+    [SerializeField] int green;
+    [SerializeField] int brown;
+
+    private void Start()
+    {
+        PlayerPrefs.SetInt(PrefsKeys.creditsKey, credits);
+        PlayerPrefs.SetInt(PrefsKeys.homeResourcesKeys[ResourceTypes.Purple], purple);
+        PlayerPrefs.SetInt(PrefsKeys.homeResourcesKeys[ResourceTypes.Red], red);
+        PlayerPrefs.SetInt(PrefsKeys.homeResourcesKeys[ResourceTypes.Blue], blue);
+        PlayerPrefs.SetInt(PrefsKeys.homeResourcesKeys[ResourceTypes.Green], green);
+        PlayerPrefs.SetInt(PrefsKeys.homeResourcesKeys[ResourceTypes.Brown], brown);
+    }
+}
+#endif
